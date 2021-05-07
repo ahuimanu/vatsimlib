8b8058c7ef19af9025381780af57df6ea32c2170
Jeffry Babb <ahuimanu@gmail.com>
2021-05-07 17:20:29 -0500

    updated repo

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,19 +3,20 @@ import pytest
 
 from ..models.models import VatsimGeneral
 
+
 def test_datetime_from_iso_timestamp():
-	# arrange
-	t = VatsimGeneral('1',						# version
-        			  '1',						# reload
-        			  '00000',					# update
-        			  '2021-04-27T18:39:55',	# timestamp
-					  1,						# connected clients
-					  1, 						# unique clients
+    # arrange
+    t = VatsimGeneral(
+        "1",  # version
+        "1",  # reload
+        "00000",  # update
+        "2021-04-27T18:39:55",  # timestamp
+        1,  # connected clients
+        1,  # unique clients
     )
-	# act
-	dt = t.get_datetime()
-	print(dt.timetz.__repr__)
+    # act
+    dt = t.get_datetime()
+    print(dt.timetz.__repr__)
 
-	# assert
-	assert dt == datetime.fromisoformat('2021-04-27T18:39:55')
-
+    # assert
+    assert dt == datetime.fromisoformat("2021-04-27T18:39:55")
